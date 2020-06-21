094c76213deee66457d538b16433cd4881fa4b9b
Zineb Kabbab <zineb.kabbab.03@gmail.com>
2020-06-21 02:34:15 +0200

    third commit taxonomy

--- wp-content/themes/Kabbab_Zineb_theme/functions.php
+++ wp-content/themes/Kabbab_Zineb_theme/functions.php
@@ -1,5 +1,44 @@
 <?php
 
+// Register Custom Taxonomy
+function criteres() {
+
+	$labels = array(
+		'name'                       => _x( 'Criteres', 'Taxonomy General Name', 'text_domain' ),
+		'singular_name'              => _x( 'Critere', 'Taxonomy Singular Name', 'text_domain' ),
+		'menu_name'                  => __( 'Criteres', 'text_domain' ),
+		'all_items'                  => __( 'Toutes criteres', 'text_domain' ),
+		'parent_item'                => __( 'Parent critere', 'text_domain' ),
+		'parent_item_colon'          => __( 'Parent Item:', 'text_domain' ),
+		'new_item_name'              => __( 'New Critere Name', 'text_domain' ),
+		'add_new_item'               => __( 'Add New Critere', 'text_domain' ),
+		'edit_item'                  => __( 'Edit Critere', 'text_domain' ),
+		'update_item'                => __( 'Update Critere', 'text_domain' ),
+		'view_item'                  => __( 'View Critere', 'text_domain' ),
+		'separate_items_with_commas' => __( 'Separate items with commas', 'text_domain' ),
+		'add_or_remove_items'        => __( 'Add or remove items', 'text_domain' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
+		'popular_items'              => __( 'Popular Criteres', 'text_domain' ),
+		'search_items'               => __( 'Search Criteres', 'text_domain' ),
+		'not_found'                  => __( 'Not Found', 'text_domain' ),
+		'no_terms'                   => __( 'No criteres', 'text_domain' ),
+		'items_list'                 => __( 'criteres list', 'text_domain' ),
+		'items_list_navigation'      => __( 'criteres list navigation', 'text_domain' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => false,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'criteres', array( 'logements' ), $args );
+
+}
+add_action( 'init', 'criteres', 0 );
+
 function monTheme_menu_class ($classes){
 $classes[] = 'nav-item';
 return $classes;
